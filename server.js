c8be267fb5c13751e141823a6a90fa9f9a4d3550
DivyanshuJ <divyanshu31dj@gmail.com>
2017-02-19 01:55:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -75,8 +75,8 @@ app.get('/', function (req, res) {
 
 app.get('/:articleName',function(req,res)
 {
-    var articleName=req.params.articleName;
-    res.send(createTemplate(articles[articleName]));
+    var articlename=req.params.articleName;
+    res.send(createTemplate(articles[articlename]));
 });
 
 app.get('/ui/style.css', function (req, res) {
