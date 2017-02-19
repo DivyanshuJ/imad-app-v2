e3ee1b420cca14aa7587e5070711dacd94fbe9cf
DivyanshuJ <divyanshu31dj@gmail.com>
2017-02-19 15:34:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,10 +9,10 @@ app.use(morgan('combined'));
 var articles=
 {
     'article-one':{
-    tit: "Article One | Divyanshu Jha",
+    title: "Article One | Divyanshu Jha",
     heading: "Article One",
     date: "Feb 13,2017",
-    cont: `<p>
+    content: `<p>
             This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article. This is the content of my first article.
         </p>
         <p>
@@ -21,18 +21,18 @@ var articles=
 }
 ,
 'article-two':{
-    tit: "Article Two | Divyanshu Jha",
+    title: "Article Two | Divyanshu Jha",
     heading: "Article Two",
     date: "Feb 14,2017",
-    cont: `<p>
+    content: `<p>
             This is the content of my second article.
         </p>`
 },
 'article-three':{
-    tit: "Article Three | Divyanshu Jha",
+    title: "Article Three | Divyanshu Jha",
     heading: "Article Three",
     date: "Feb 18,2017",
-    cont: `<p>
+    content: `<p>
             This is the content of my third article.
         </p>`
 }
@@ -40,14 +40,14 @@ var articles=
 
 function createTemplate(data)
 {
-    var tit=data.tit;
+    var title=data.title;
     var heading=data.heading;
     var date=data.date;
-    var cont=data.cont;
+    var content=data.content;
 var htmlTemplate=`<html>
     <head>
         <title>
-            ${tit}
+            ${title}
         </title>
             <meta name="viewport" content="width-device-width,initial-scale=1/">
                 <link href="/ui/style.css" rel="stylesheet" />
@@ -62,7 +62,7 @@ var htmlTemplate=`<html>
             ${heading}
         </h3>
         <div>${date}</div>
-        ${cont}
+        ${content}
         </div>
     </body>
 </html>`
@@ -80,8 +80,8 @@ app.get('/counter',function(req,res){
 
 app.get('/:articleName',function(req,res)
 {
-    var articlename=req.params.articleName;
-res.send(createTemplate(articles[articlename]));
+    var articleName=req.params.articleName;
+    res.send(createTemplate(articles[articleName]));
 });
 
 app.get('/ui/style.css', function (req, res) {
