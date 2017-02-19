4fe4b3d723f74a2ecee4a3bd542a77e7a8ccf59f
DivyanshuJ <divyanshu31dj@gmail.com>
2017-02-19 14:34:25 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -87,15 +87,11 @@ app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
 
-app.get('/favicon.ico', function (req, res) {
-  console.log("hello to you!!!");
-});
-
 app.get('/ui/main.js', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'main.js'));
 });
 var counter=0;
-app.get('/counter',function(req,res){
+app.get('/coun',function(req,res){
     counter=counter+1;
     res.send(counter.toString());
 });
