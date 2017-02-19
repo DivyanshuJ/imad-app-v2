e0cd9130a18383cf1eb754a2ba602e01d3dd31ea
DivyanshuJ <divyanshu31dj@gmail.com>
2017-02-19 23:18:29 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ submit.onclick=function()
         {
             if(request.status===200)
             {
-                var names=request.responeText;  
+                var names=request.responseText;  
                 names=JSON.parse(names);
                 var list='';
                 for(i=0;i<names.length;i++)
