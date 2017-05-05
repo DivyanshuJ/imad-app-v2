211df7392e4417ab7b0c2816a9e583a9455773a9
DivyanshuJ <divyanshu31dj@gmail.com>
2017-05-05 05:35:34 -0700

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -72,6 +72,9 @@ return htmlTemplate;
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+app.get('/media', function (req, res) {
+  res.sendFile(path.join(__dirname, 'ui', 'multimedia.html'));
+});
 var counter=0;
 app.get('/counter',function(req,res){
     counter=counter+1;
